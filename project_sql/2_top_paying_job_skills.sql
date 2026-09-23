bb00312d9b0cce2067a3ef542d237c2fd5aa43ec
enricops <enricops.32@gmail.com>
2026-09-24 00:02:10 +0700

    sql update

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -9,16 +9,11 @@ WITH top_paying_jobs AS (
     LEFT JOIN
         company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_title_short = 'Data Analyst' AND
-        job_location = 'Anywhere' AND
-        salary_year_avg IS NOT NULL OR
-        job_title_short = 'Business Analyst' AND
-        job_location = 'Anywhere' AND
-        salary_year_avg IS NOT NULL
+        (job_title_short = 'Data Analyst' OR job_title_short = 'Business Analyst')
+        AND job_location = 'Anywhere'
+        AND salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
-    LIMIT
-        10
 )
 
 SELECT 
